d979b20bbd33441a607cc21fd51b533b169ea70c
alfarruq <mengboyevfarrux01@gmail.com>
2025-04-18 14:41:28 +0500

    changed app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
+// import logo from './assets/img/logo.png'
+import "./App.css";
 
-
-import './App.css'
+let isLight =
+  new Date().getHours() > 6 && new Date().getHours() < 18 ? true : false;
 
 function App() {
- 
-
   return (
-   <div>
-      <h1>Assalamu alaykum</h1>
-      <h2>Nima gap</h2>
-   </div>
- 
-  )
+    <div>
+      <h1 className={isLight ? "light" : "dark"}>
+        Nima gap? {new Date().getHours()}{" "}
+      </h1>
+      <h1>Random Number {(Math.random() * 100).toFixed()} </h1>
+
+      {isLight ? <h3>Talaba</h3> : <mark>Talaba emas</mark>}
+    </div>
+  );
 }
 
-export default App
+export default App;
